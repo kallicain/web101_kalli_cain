3edc69d0bd5f75d20258e5386ec55bb723f4047e
kallicain <kallihicks@gmail.com>
2023-02-12 17:31:03 -0600

    added onclick and responsive

---
+++ scripts/contact.js
@@ -0,0 +1,20 @@
+const buttonElement = document.querySelector('button')
+const emailInput = document.querySelector('#email')
+const textbox = document.querySelector('.textbox')
+const school= document.querySelector('#school')
+const work= document.querySelector('#work')
+const resume= document.querySelector('#resume')
+const yes= document.querySelector('#yes')
+const no= document.querySelector('#no')
+const date= document.querySelector('#date')
+const resultsSection = document.getElementById('results')
+
+console.log(resultsSection);
+console.log(buttonElement);
+
+const submitForm = () => {
+  console.log('Form Submitted')
+  alert('Form Submitted')
+  
+}
+
